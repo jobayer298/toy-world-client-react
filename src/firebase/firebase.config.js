783f2b6e9b4e997199cadda1c6c90f446f082433
Md. Jobayer Hossain <jobayerhossain298@gmail.com>
2023-05-20 16:23:40 +0600

    firebase config added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -10,10 +10,10 @@ const firebaseConfig = {
   projectId:import.meta.env.VITE_PROJECTID,
   storageBucket:import.meta.env.VITE_STORAGEBUCKET,
   messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
-  appId:import.meta.env.VITE_APPID
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
- 
+
 export default app;
